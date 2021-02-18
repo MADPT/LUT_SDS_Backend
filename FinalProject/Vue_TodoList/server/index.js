3ca304a3ab136e908ff14df66a2340ad1c0877e6
MADPT <miguelftangelo@gmail.com>
2021-02-18 17:32:42 +0200

    Update server for deployment

--- FinalProject/Vue_TodoList/server/index.js
+++ FinalProject/Vue_TodoList/server/index.js
@@ -9,6 +9,15 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/todos', todosRouter);
 
+// Handle production
+if (process.env.NODE_ENV === 'production ') {
+	// Static folder
+	app.use(express.static(__dirname + '/public'));
+
+	// Handle SPA
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`);
 });
