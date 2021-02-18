2c5f44be58b8ab661943e0dffcd155f463621a6a
MADPT <miguelftangelo@gmail.com>
2021-02-18 16:42:28 +0200

    Add cors

--- FinalProject/Vue_TodoList/server/index.js
+++ FinalProject/Vue_TodoList/server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const todosRouter = require('./routes/api/todos');
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use('/api/todos', todosRouter);
 
 app.listen(port, () => {
